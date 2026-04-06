f3b928dc625a5d6b869ce36efc033bdc3d59d713
Kamil <blaszkowkamil@yahoo.com>
2026-04-06 22:58:58 +0200

    Leaderboard Interface
    The leaderboard have now a interface, with working buttons to switch between the solo and coop leaderboards, a refresh button to fetch the most recent database informations, and a 'go back' button.
    
    For now there is no ways to add the player score to the database, because the end game wasn't implemented yet. But the leaderboard was still tested with false leaderboards/values.

--- core/src/main/java/io/github/Chino/LodeRunner/GameInterface/Interface/LeaderboardScreen.java
+++ core/src/main/java/io/github/Chino/LodeRunner/GameInterface/Interface/LeaderboardScreen.java
@@ -1,11 +1,19 @@
 package io.github.Chino.LodeRunner.GameInterface.Interface;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import io.github.Chino.LodeRunner.GameInterface.GDXMain;
@@ -20,19 +28,27 @@ public class LeaderboardScreen implements Screen {
     private SpriteBatch batch;
     private Stage uiStage;
 
+    private final Skin skin = new Skin(Gdx.files.internal("textbuttonskin/textbuttonSkin.json"));
+
+    private Table tableOfLeaderboardLabel;
+
     private LeaderboardHandler leaderboardHandler;
 
     private Texture backgroundTexture;
     private int currentBackgroundXOffset = 0;
     private boolean isBackgroundMovingLeft = false;
 
+    private boolean isOnSoloLeaderboard = true;
+
     public LeaderboardScreen(GDXMain main){
         this.main = main;
 
         initBackground();
+        initInterface();
 
-        // TODO put connection to database in a thread
         this.leaderboardHandler = new LeaderboardCreator().createLeaderboardHandler();
+        // by default show the solo leaderboard
+        setupSoloLeaderboard();
     }
 
     private void initBackground(){
@@ -40,6 +56,70 @@ public class LeaderboardScreen implements Screen {
         this.uiStage = new Stage(this.screenViewport);
 
         this.backgroundTexture = new Texture("menuBackground.png");
+    }
+
+    private void initInterface(){
+        Gdx.input.setInputProcessor(this.uiStage);
+
+        Table tableOfContent = new Table();
+        tableOfContent.setFillParent(true);
+
+        Table tableOfButtonsOnTop = new Table();
+
+        TextButton soloButton = new TextButton("Solo", skin);
+        TextButton coopButton = new TextButton("Coop", skin);
+
+        tableOfButtonsOnTop.add(soloButton).padRight(15);
+        tableOfButtonsOnTop.add(coopButton);
+        tableOfContent.add(tableOfButtonsOnTop).expandX().fillX().top().padBottom(10).row();
+        
+        this.tableOfLeaderboardLabel = new Table();
+        tableOfContent.add(this.tableOfLeaderboardLabel).expand().fill().center().row();
+        
+        Table tableOfButtonsOnBottom = new Table();
+
+        TextButton goBackButton = new TextButton("Go Back", skin);
+        TextButton refreshButton = new TextButton("Refresh", skin);
+
+        tableOfButtonsOnBottom.add(goBackButton).padRight(15);
+        tableOfButtonsOnBottom.add(refreshButton);
+        tableOfContent.add(tableOfButtonsOnBottom).expandX().fillX().bottom().padBottom(10);
+
+        soloButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent e, float x, float y){
+                setupSoloLeaderboard();
+                isOnSoloLeaderboard = true;
+            }
+        });
+
+        coopButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent e, float x, float y){
+                setupCoopLeaderboard();
+                isOnSoloLeaderboard = false;
+            }
+        });
+
+        goBackButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent e, float x, float y){
+                main.getMenuScreen().setMovingBackgroundInfo(currentBackgroundXOffset, isBackgroundMovingLeft);
+                main.setScreen(main.getMenuScreen());
+            }
+        });
+
+        refreshButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent e, float x, float y){
+                if(isOnSoloLeaderboard){
+                    setupSoloLeaderboard();
+                }else{
+                    setupCoopLeaderboard();
+                }
+            }
+        });
+        this.uiStage.addActor(tableOfContent);
     }
 
     @Override
@@ -56,6 +136,11 @@ public class LeaderboardScreen implements Screen {
         this.batch.begin();
         this.batch.draw(this.backgroundTexture, this.currentBackgroundXOffset, -200);
         this.batch.end();
+
+        this.uiStage.act(delta);
+        this.uiStage.draw();
+
+        this.uiStage.setDebugAll(true);
     }
 
     private void moveBackground(){
@@ -75,6 +160,35 @@ public class LeaderboardScreen implements Screen {
         this.currentBackgroundXOffset = currentXOffset;
         this.isBackgroundMovingLeft = isMovingLeft;
     } 
+
+    private void setupSoloLeaderboard(){
+        // tableOfLeaderboardLabel.add(new Label("Player 1", skin)).expandY().fillY().center().row();
+
+        this.tableOfLeaderboardLabel.clear();
+
+        ArrayList<Object> leaderboardDatabaseInformations = this.leaderboardHandler.fetchSoloLeaderboardDatabase(10);
+
+        String currentInformations = "";
+        for (int i = 0; i < leaderboardDatabaseInformations.size(); i += 2) {
+            currentInformations += (i / 2 + 1) + " : " + leaderboardDatabaseInformations.get(i) + " Score : " + leaderboardDatabaseInformations.get(i + 1);
+            this.tableOfLeaderboardLabel.add(new Label(currentInformations, skin)).expandY().fillY().row();
+
+            currentInformations = "";
+        }
+    }
+    private void setupCoopLeaderboard(){
+        this.tableOfLeaderboardLabel.clear();
+
+        ArrayList<Object> leaderboardDatabaseInformations = this.leaderboardHandler.fetchCoopLeaderboardDatabase(10);
+
+        String currentInformations = "";
+        for (int i = 0; i < leaderboardDatabaseInformations.size(); i += 2) {
+            currentInformations += (i / 2 + 1) + " : " + leaderboardDatabaseInformations.get(i) + " Score : " + leaderboardDatabaseInformations.get(i + 1);
+            this.tableOfLeaderboardLabel.add(new Label(currentInformations, skin)).expandY().fillY().row();
+
+            currentInformations = "";
+        }
+    }
 
     @Override
     public void resize(int width, int height) {
